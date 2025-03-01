ee6f66d749577bf39c2e816a336c7009bd700898
ammar <ammarabdelateif23@gmail.com>
2025-03-01 14:57:06 +0200

    Product-Sales-Analysis III

---
+++ Random_problems/2-product-sales-analysis-iii.sql
@@ -0,0 +1,17 @@
+SELECT 
+    product_id,
+    year AS first_year,
+    quantity,
+    price
+FROM (
+    SELECT 
+        product_id,
+        year,
+        quantity,
+        price,
+        rank() OVER (PARTITION BY product_id ORDER BY year) AS rnk
+    FROM 
+        Sales
+) AS ranked_sales
+WHERE 
+    rnk = 1;
